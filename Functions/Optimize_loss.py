0778e4abe6cc093812e7ef74143fd135d2cbd127
Seyda Aydin <42001182+SeydaBA@users.noreply.github.com>
2025-10-25 16:00:46 +0200

    Update Optimize_loss.py

--- Functions/Optimize_loss.py
+++ Functions/Optimize_loss.py
@@ -1,4 +1,4 @@
-def optimize_factual_loss(model_prediction, optimizer_factual, train_loader_target, device, max_epochs_phase_2, phi_target_dict): #phi_target_dict comes from optimize_ipm
+def loss_function(model_prediction, optimizer_factual, train_loader_target, device, max_epochs_phase_2, phi_target_dict): #phi_target_dict comes from optimize_ipm
     model_prediction.train()
 
     for epoch in range(max_epochs_phase_2):
